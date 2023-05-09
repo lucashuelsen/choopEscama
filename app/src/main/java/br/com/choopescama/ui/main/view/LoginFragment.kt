92d8626bcb02e3bbc6ee4a1c22a55727fef86736
lucashuelsen <lucas.huelsen@mc1.com.br>
2023-05-09 15:33:28 -0300

    Foi realizado a configuração do firebase auth - Implementado teste para criação de usuário para validar a configuração.

--- app/src/main/java/br/com/choopescama/ui/main/view/LoginFragment.kt
+++ app/src/main/java/br/com/choopescama/ui/main/view/LoginFragment.kt
@@ -2,30 +2,48 @@ package br.com.choopescama.ui.main.view
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import br.com.choopescama.databinding.ActivityLoginBinding
 import androidx.databinding.DataBindingUtil
 import br.com.choopescama.R
 import br.com.choopescama.ui.main.viewmodel.LoginViewModel
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
 
 
 class LoginFragment : AppCompatActivity() {
     private var mBinding : ActivityLoginBinding? = null
     private var mViewModel: LoginViewModel? = null
+    var listener : OnCompleteListener<AuthResult>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setupBinding()
+        setupSuccessListener()
         setupViewModel()
+        setupBinding()
+    }
+
+    private fun setupSuccessListener() {
+        listener = OnCompleteListener<AuthResult> {
+            if(it.isSuccessful){
+                Toast.makeText(this, it.exception?.message,Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(this, it.exception?.message,Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
     private fun setupBinding(){
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
         mBinding?.lifecycleOwner = this
+        mBinding?.viewModel = mViewModel
     }
 
     private fun setupViewModel(){
-        mViewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+        mViewModel = ViewModelProvider(
+            this,
+            LoginViewModel.LoginViewModelFactory(listener)
+            ).get(LoginViewModel::class.java)
     }
 }
--- app/src/main/java/br/com/choopescama/ui/main/viewmodel/LoginViewModel.kt
+++ app/src/main/java/br/com/choopescama/ui/main/viewmodel/LoginViewModel.kt
@@ -1,7 +1,42 @@
 package br.com.choopescama.ui.main.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
 
-class LoginViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class LoginViewModel(val listener: OnCompleteListener<AuthResult>?) : ViewModel() {
+    private var auth: FirebaseAuth? = null
+    var email = MutableLiveData<String>()
+    var password = MutableLiveData<String>()
+
+    init {
+        auth = FirebaseAuth.getInstance()
+    }
+
+    fun onClickButton() {
+        if (listener != null) {
+            auth?.createUserWithEmailAndPassword(
+                email.value ?: "",
+                password.value ?: ""
+            )?.addOnCompleteListener(listener)
+        }
+    }
+
+    class LoginViewModelFactory(val listener: OnCompleteListener<AuthResult>?) :
+        ViewModelProvider.NewInstanceFactory() {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            //Local variables
+            val viewModel = when (modelClass) {
+                LoginViewModel::class.java -> {
+                    LoginViewModel(listener) as T
+                }
+                else -> super.create(modelClass)
+            }
+
+            return viewModel
+        }
+    }
 }
